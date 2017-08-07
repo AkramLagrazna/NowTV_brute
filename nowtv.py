2f2766ab57af52e6479a430712171ee5a46665a9
Akram Lagrazna <lagrazna@outlook.it>
2017-08-08 01:28:40 +0200

    Update nowtv.py

--- nowtv.py
+++ nowtv.py
@@ -13,6 +13,8 @@ def get_html():
     if lol == '':
         with open("Hitties.txt","a") as myfile:
             myfile.write(''.join(characters))
+            myfile.write('\n')
+            
 
 letters = ['a','b','c','d','e','f','g','h','i','l','m','n','o','p','q','r','s','t','u','v','w','y','k','z','1','2','3','4','5','6','7','8','9','0']
 characters = ['0','0','0','0','0','0']
